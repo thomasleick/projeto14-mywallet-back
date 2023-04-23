0ee1659fb87b7e40f9a74e5c411e0c67fe2be741
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-04-22 23:56:29 -0300

    frontend add to allowedOrigins

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://www.google.com', 
     '127.0.0.1:5500', 
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://projeto14-mywallet-front-mocha.vercel.app/'
 ]
 
 module.exports = allowedOrigins
