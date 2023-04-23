0289ec9001ac59b393d2e7d74b9eb865877fdb73
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-04-23 13:05:20 -0300

    put transaction

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,6 +1,6 @@
 const { query } = require('express');
 const Transaction = require('../models/Transaction');
-const User = require('../models/User')
+const User = require('../models/User');
 
 const postTransaction = async (req, res) => {
   const { type } = req?.params;
@@ -31,7 +31,7 @@ const postTransaction = async (req, res) => {
   catch (err) {
     return res.status(500).json({ message: err.message });
   }
-}
+};
 
 const getTransaction = async (req, res) => {
   const userId = req?.headers?.id;
@@ -39,7 +39,7 @@ const getTransaction = async (req, res) => {
   if (!userId) return res.sendStatus(400);
   try {
     const transactions = await Transaction.find({ userId });
-    return res.json(transactions); 
+    return res.json(transactions);
   } catch (err) {
     console.error(err);
     return res.sendStatus(500);
@@ -70,7 +70,29 @@ const deleteTransaction = async (req, res) => {
 };
 
 const putTransaction = async (req, res) => {
-  return
-}
+  const userId = req?.headers?.id;
+  const transactionId = req?.params?.id;
+  const { value, description } = req?.body;
+
+  if (!userId || !transactionId) return res.sendStatus(400);
+
+  try {
+    // Update transaction
+    await Transaction.findOneAndUpdate(
+      { _id: transactionId, userId },
+      { value, description },
+      { new: true, runValidators: true }
+    );
+
+    return res.sendStatus(204);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return res.status(422).send({ message: err.message });
+    }
+
+    console.error(err);
+    return res.sendStatus(500);
+  }
+};
 
 module.exports = { postTransaction, getTransaction, deleteTransaction, putTransaction };
